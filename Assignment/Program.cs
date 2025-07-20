da005bad5a3dbf4049646b939b0387cb2bd38fa2
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2025-07-20 08:42:25 +0300

    All Project Demo & Assignment

---
+++ Assignment/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment
+{
+
+
+    #region Q01
+
+    // 1.Define a struct "Person" with properties "Name" and "Age".
+    // Create an array of three "Person" objects and populate it with data.Then,
+    // write a C# program to display the details of all the persons in the array.
+
+    struct Person
+    {
+
+        public int Age { get; set; }
+        public string? Name { get; set; }
+
+        public Person(int age, string? name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+
+            return $"Name: {Name}, Age: {Age}";
+        }
+
+    }
+
+    #endregion
+
+    #region Q02
+    // Create a struct called "Point" to represent a 2D point with properties "X" and "Y".
+    // Write a C# program that takes two points as input from the user and calculates the distance between them.
+
+    struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double DistanceBetweenPoints(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+        }
+
+    }
+
+    #endregion
+
+    #region Q03
+    // 3. Create a struct called "Person" with properties "Name" and "Age".
+    // Write a C# program that takes details of 3 persons as input
+    // from the user and displays the name and age of the oldest person.
+    struct Person03
+    {
+
+        public int Age { get; set; }
+        public string? Name { get; set; }
+
+        public Person03(int age, string? name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public Person03 GetOldest(Person03[] person)
+        {
+            Person03 oldPerson = person[0];
+            if (person?.Length is not null)
+            {
+                for (int i = 0; i < person.Length; i++)
+                {
+                    if (person[i].Age > oldPerson.Age)
+                    {
+                        oldPerson = person[i];
+                    }
+                }
+            }
+            return oldPerson;
+        }
+
+    }
+
+
+
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q01
+            // 1.Define a struct "Person" with properties "Name" and "Age".
+            // Create an array of three "Person" objects and populate it with data.Then,
+            // write a C# program to display the details of all the persons in the array.
+
+            Person[] persons = new Person[3];
+
+            Console.WriteLine("Enter details for 3 persons:");
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine($"Enter details for person {i + 1}:");
+                for (int j = 0; j < 1;)
+                {
+                    Console.Write("Name: ");
+                    string? name = Console.ReadLine();
+
+                    Console.Write("Age: ");
+                    bool isAgeValid = int.TryParse(Console.ReadLine(), out int age);
+
+                    if (isAgeValid && name?.Length > 0)
+                    {
+                        persons[i] = new Person(age, name);
+                        j++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid input. Please enter a valid name and age For Person {i + 1} again.");
+                    }
+                }
+
+            }
+
+            Console.Clear();
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine($"Person {i + 1}\nName: {persons[i].Name} , Age: {persons[i].Age}");
+            }
+
+            #endregion
+
+            #region Q02
+            // Create a struct called "Point" to represent a 2D point with properties "X" and "Y".
+            // Write a C# program that takes two points as input from the user and calculates the distance between them.
+            Point point = new Point();
+
+            Console.WriteLine("\nEnter X1 :");
+            bool isValidInputX01 = double.TryParse(Console.ReadLine(), out double x1);
+
+            Console.WriteLine("Enter Y1 :");
+            bool isValidInputY01 = double.TryParse(Console.ReadLine(), out double y1);
+
+            Console.WriteLine("Enter X2 :");
+            bool isValidInputX02 = double.TryParse(Console.ReadLine(), out double x2);
+
+            Console.WriteLine("Enter Y2 :");
+            bool isValidInputY02 = double.TryParse(Console.ReadLine(), out double y2);
+
+            if (isValidInputX01 && isValidInputY01 && isValidInputX02 && isValidInputY02)
+            {
+                Point p1 = new Point(x1, y1);
+                Point p2 = new Point(x2, y2);
+                double distance = point.DistanceBetweenPoints(p1, p2);
+                Console.WriteLine($"The distance between the points ({p1.X}, {p1.Y}) and ({p2.X}, {p2.Y}) is: {distance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Enter Valid Inputs.");
+            }
+
+            #endregion
+
+
+            #region Q03
+            // Create a struct called "Person" with properties "Name" and "Age". 
+            // Write a C# program that takes details of 3 persons as input from
+            // the user and displays the name and age of the oldest person.
+
+            Person03[] persons03 = new Person03[3];
+
+            Console.WriteLine("Enter details for 3 persons:");
+
+            for (int i = 0; i < persons03.Length; i++)
+            {
+                Console.WriteLine($"Enter details for person {i + 1}:");
+                for (int j = 0; j < 1;)
+                {
+                    Console.Write("Name: ");
+                    string? name = Console.ReadLine();
+
+                    Console.Write("Age: ");
+                    bool isAgeValid = int.TryParse(Console.ReadLine(), out int age);
+
+                    if (isAgeValid && name?.Length > 0)
+                    {
+                        persons03[i] = new Person03(age, name);
+                        j++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid input. Please enter a valid name and age For Person {i + 1} again.");
+                    }
+                }
+            }
+
+            Person03 temp = new Person03();
+            Person03 oldest = temp.GetOldest(persons03);
+
+            Console.WriteLine($"The Oldest Person : \nName : {oldest.Name} , Age : {oldest.Age}");
+
+
+
+            #endregion
+        }
+    }
+}
---
+++ Demo/Encapsulation/Employee.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+
+namespace Demo.Encapsulation
+{
+
+    struct Employee
+    {
+        // Encapsulation ==> Separates Data definition[Attributes | Fields] from its Use [Getters & Setters]
+        // Hiding the implementation details of a class or struct from the outside world,
+
+        #region Attribute
+
+        private int id;
+        private string? name;
+        private decimal salary;
+        // private decimal deduction; // Derived Attribute, not stored in memory, calculated on the fly
+
+        #endregion
+
+        #region 1.Applying Encapsulation thorugh Getters & Setters [Old Approach]
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public void SetId(int value)
+        {
+            id = value > 1000 && value < 10_000 ? value : 9999; // Data Validation
+        }
+        #endregion
+
+        #region Properties
+        // Applying Encapsulation through Properties [New Approach] Recommended
+
+        /// 2.1 Full Properties
+        /// Called Full because you create both Fields and Properties
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value > 1000 && value < 10_000 ? value : 9999; // Data Validation
+            }
+        }
+
+        public string? Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = ValidateName(value);
+            }
+            /// init
+            /// {
+            /// 
+            ///     // init is used to allow setting the property only during object initialization
+            ///     // init make you Initialize in Object Initializer 
+            ///     // init is Used instead of set 
+            ///     // and not allow to change the value after object creation
+            /// 
+            /// }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = ValidateSalary(value); // Data Validation
+            }
+        }
+
+        // Read-Only Property
+        public decimal Deduction
+        {
+            get
+            {
+                return salary * .2M;
+            }
+        }
+
+        // 2.2 Automatic Properties
+        // Compiler will Generate Backing Fields 'Hidden Private Attributes'
+
+        private int Age { get; set; } // Auto-implemented property, no backing field
+                                      // int <age>_backingField; // Compiler generates this field automatically
+
+        #endregion
+       
+        #region Code Snippet For Properties
+        // propfull: is a Code Snippet for [Full Property]
+
+        private string? address;
+        public string? Address
+        {
+            get { return address; }
+            set { address = value; }
+
+        }
+
+        // prop: is a Code Snippet for [Automatic Property]
+
+        public int MyProperty { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Employee(int id, string? name, decimal salary, int age)
+        {
+            this.id = id;
+            this.name = ValidateName(name);
+            this.salary = ValidateSalary(salary);
+            this.Age = age; // Using Property Setter
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"Id : {this.id}\nName : {this.name}\nSalary : {this.salary}";
+        }
+
+        private string? ValidateName(string? name)
+        {
+            return name is null ? null : name?.Length < 20 ? name : name?.Substring(0, 10);
+        }
+
+        private decimal ValidateSalary(decimal salary)
+        {
+            return salary > 5000 ? salary : 5000; // Data Validation
+        }
+
+        #endregion
+
+        #region Notes
+        //  If You Make Validation in the Setters of the Properties You Must Nake This Validatoin in Constructors
+        // You can Make Private Method To Validate the Data and Call it in the Properties Setters and in the Constructors 
+        #endregion
+
+    }
+
+}
